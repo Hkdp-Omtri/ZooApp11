6b16dcca866c72279f0437983f46a301c2376549
Homesh_omtri <xyxyx@gmail.com>
2021-10-27 11:36:51 -0500

    adding files

---
+++ src/main/java/edu/nwmissouri/zoo11group/ZooAppApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo11group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/zoo11group/ZooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo11group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
