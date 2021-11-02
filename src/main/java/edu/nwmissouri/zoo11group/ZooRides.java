a15ea506684186f0a48587d06fc5b7437503d123
SARATH ARREPU <89147362+SarathArrepu@users.noreply.github.com>
2021-11-02 16:25:13 -0500

    Create ZooRides.java

---
+++ src/main/java/edu/nwmissouri/zoo11group/ZooRides.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo11group;
+
+/**
+ *
+ * @author S545027
+ */
+public abstract class ZooRides {
+    private String name;
+    private String ticketType;
+    private int noofcustomers;
+    
+    public void ticketprice(){
+        
+}
+    public void noofseats(){
+    }
+}
