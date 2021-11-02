25ad9d98ba2fc8dc738b139c220687416c175b3b
SARATH ARREPU <89147362+SarathArrepu@users.noreply.github.com>
2021-11-02 16:26:43 -0500

    Create ZooAdventure.java

---
+++ src/main/java/edu/nwmissouri/zoo11group/ZooAdventure.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo11group;
+
+/**
+ *
+ * @author S545027
+ */
+public abstract class ZooAdventure {
+    private String adventurename;
+    private int noofcustomers;
+    
+    public void ageLimit(){
+        
+}
+    public void idproof(){
+    }
+    
+    public void safetymeasures(){
+    }
+    
+}
