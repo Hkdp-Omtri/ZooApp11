b4aea32ed767f2b3b23f37f1ebb541b02459c2e7
MPravallika6 <s545254@nwmissouri.edu>
2021-11-02 22:00:09 -0500

    changes in rollerCosters method

--- src/main/java/edu/nwmissouri/zoo11group/RollerCoasterAdventure.java
+++ src/main/java/edu/nwmissouri/zoo11group/RollerCoasterAdventure.java
@@ -8,6 +8,39 @@ package edu.nwmissouri.zoo11group;
  *
  * @author S545254
  */
-public class RollerCoasterAdventure {
+public class RollerCoasterAdventure extends ZooAdventure {
+    private String nameOfThePerson;
+    private boolean isCondition;
+     /**
+     *
+     */
+    @Override
+    public void ageLimit(){
+    }
+    /**
+     * 
+     */
+    @Override
+    public void idProof(){
+        
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void safetyMeasures(){
+        
+    }
+    public boolean condition(){
+        System.out.println("checking the condition is true or false if false mention danger");
+        
+        return false;
+    }
+    public void color(){
+        System.out.println("Color of the roller coster will mentioned as per changes");
+        
+    
+    }
     
 }
