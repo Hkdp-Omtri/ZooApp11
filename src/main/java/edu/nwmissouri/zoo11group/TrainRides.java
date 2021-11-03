64538baad28ca52e6b9bc86b38c43b8935942fa6
VenkataPrabhakar <89261443+VenkataPrabhakar@users.noreply.github.com>
2021-11-02 21:16:34 -0500

    Adding classes with methods

--- src/main/java/edu/nwmissouri/zoo11group/TrainRides.java
+++ src/main/java/edu/nwmissouri/zoo11group/TrainRides.java
@@ -9,6 +9,34 @@ package edu.nwmissouri.zoo11group;
  * @author S545407
  */
 public class TrainRides extends ZooRides{
+   private String reservedNo; 
+   private double speed;
+   private double time;
+   private double distance;
+   
+    /**
+     *
+     */
+    @Override
+    public void ticketprice(){
+        
+}
+
+    /**
+     *
+     */
+    @Override
+    public void noofseats(){
+    }
+    public void trainRides(){
     
-    
+    }
+    public String seatNumber(){
+        
+       return null;
+   }
+    public double speedCalculation(){
+        
+       return 0;
+   }
 }
