71c329ba1cc714e675b0f1d098653907ac8fd6ed
Gandra Alekhya <36558540+GandraAlekhya@users.noreply.github.com>
2021-11-02 21:03:12 -0500

    Create RopeRidesAdventure.java

---
+++ src/main/java/edu/nwmissouri/zoo11group/RopeRidesAdventure.java
@@ -0,0 +1,38 @@
+package edu.nwmissouri.zoo11group;
+
+/**
+ *
+ * @author Alekhya Gandra
+ */
+public class RopeRidesAdventure extends ZooAdventure {
+    
+    int noOfMembers;
+    double speed;
+    
+   
+    
+    public int  height()
+    {
+        int height = 10 ;
+        return height;
+    }
+    public void isWinner()
+    {
+        System.out.println("The performer is winner ");
+    }
+    @Override
+    public void ageLimit()
+    {
+        System.out.println("The age limit is 22 years ");
+    }
+    @Override
+    public void idProof()
+    {
+        System.out.println("ID proof is verified");
+    }
+    @Override
+    public void safetyMeasures()
+    {
+        System.out.println("SafetyMeasures are taken");
+    }
+}
