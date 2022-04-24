a03bfecf93c54020c8ed62387cb42f995c6214ba
Homakesavadurgaprasad <89258140+Hkdp-Omtri@users.noreply.github.com>
2022-04-24 14:07:45 -0500

    Add files via upload

---
+++ Omtri_Movies/Omtri_Movies/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Omtri_Movies
+//
+//  Created by Student on 4/22/22.
+//
+
+import Foundation
+
---
+++ Omtri_Movies/Omtri_Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Omtri_Movies
+//
+//  Created by Student on 4/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
